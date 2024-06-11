c5e38c0c44d785a5d98d2bb23e0d03f0c999de2e
IliasAgaz <ilyasagaz0@gmail.com>
2024-06-11 14:46:05 +0100

    Changing schema of user table

--- 001_create_users_table.sql
+++ 001_create_users_table.sql
@@ -1,6 +1,7 @@
 -- Create a table named 'users'
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    lastname VARCHAR(50) NOT NULL,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
